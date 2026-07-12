0aff5bfd82c8e99873700a3e4196ddc7a0c87730
Atul Kumar <tiwariatul1016@gmail.com>
2026-07-12 17:20:46 +0530

    adding install.sh

---
+++ Backend/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+npm install \
+express dotenv cors helmet morgan compression \
+jsonwebtoken bcrypt \
+express-validator \
+multer \
+@prisma/client \
+pino pino-http \
+swagger-ui-express swagger-jsdoc \
+redis \
+kafkajs \
+minio \
+prom-client
+
+npm install -D \
+prisma \
+nodemon \
+jest \
+supertest \
+pino-pretty
