383fd6634630a2a673b9e9c3a77ae26f84bf2419
ashu670 <abhaylal122@gmail.com>
2026-07-13 23:11:48 +0530

    auth done

---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,52 @@
+import * as authService from '../services/authService.js';
+
+const COOKIE_OPTIONS = {
+    httpOnly : true,
+    secure : process.env.NODE_ENV === "production",
+    sameSite : 'strict',
+    maxAge : 7 * 24 * 60 * 60 * 1000
+};
+
+export const signup = async (req, res) => {
+    try{
+        const {name, email, password} = req.body;   //validation will be done before this
+        const {user, accessToken, refreshToken} = await authService.registerUser(name, email, password);
+        res.cookie('refreshToken', refreshToken, COOKIE_OPTIONS);
+        return res.status(201).json({user, accessToken});
+    }catch(err){
+        return res.status(400).json({error : err.message});
+    }
+};
+
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        const {user, accessToken, refreshToken} = await authService.loginUser(email, password);
+
+        res.cookie('refreshToken', refreshToken, COOKIE_OPTIONS);
+        return res.status(200).json({user, accessToken});
+    }catch(err){
+        return res.status(401).json({error : err.message});
+    }
+}
+
+export const refresh = async (req, res) => {
+    try{
+        const {refreshToken} = req.cookies.refreshToken;
+        if(!refreshToken) return res.status(401).json({error : 'Refresh token are missing'});
+
+        const {accessToken} = await authService.refreshAccessTokens(refreshToken);
+        return res.status(200).json({accessToken});
+    }catch(err){
+        return res.status(403).json({error : 'invalid refresh tokens'});
+    }
+};
+
+export const logout = (req, res) => {
+    res.clearCookie('refreshToken', {...COOKIE_OPTIONS, maxAge : 0});
+    return res.status(200).json({message : 'Logout succesfull'});
+};
+
+export const getProfile = (req, res) => {
+    return res.status(200).json({user : req.user});
+}
---
+++ Frontend/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing CloudBox Frontend..."
+
+npm install
+
+npm install axios react-router-dom
+
+echo "Frontend setup complete."
+echo ""
+echo "Run:"
+echo "npm run dev"
