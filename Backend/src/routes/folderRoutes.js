59fd771d8153f68101913ce2e281d1d6573ddf6c
ashu670 <abhaylal122@gmail.com>
2026-07-17 01:03:06 +0530

    fixes

--- Backend/src/routes/folderRoutes.js
+++ Backend/src/routes/folderRoutes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post('/create', authenticate, folderCon.create);
 router.get('/fetch/:id', authenticate, folderCon.fetch);
+router.delete('/delete/:id', authenticate, folderCon.deleteFolder);
 
 export default router;
