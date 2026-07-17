2bcfd653b1b1f32b9440ade670bd8032e764b2a1
ashu670 <abhaylal122@gmail.com>
2026-07-17 21:45:17 +0530

    folder repo

---
+++ Backend/src/repositories/folderRepo.js
@@ -0,0 +1,45 @@
+import { prisma } from "../config/db.js";
+
+export const findByIdAndUser = async (id, uid) => {
+    if (!id) return true;
+
+    return await prisma.folder.findFirst({
+        where: {
+            id,
+            uid
+        }
+    });
+};
+
+export const findDuplicate = async (name, pid, uid) => {
+    return await prisma.folder.findFirst({
+        where: {
+            name,
+            pid,
+            uid
+        }
+    });
+};
+
+export const create = async (data) => {
+    return await prisma.folder.create({
+        data
+    });
+};
+
+export const findChildren = async (uid, pid) => {
+    return await prisma.folder.findMany({
+        where: {
+            uid,
+            pid
+        }
+    });
+};
+
+export const deleteFolder = async (id) => {
+    return await prisma.folder.delete({
+        where: {
+            id
+        }
+    });
+};
