ee7d8299e8471ddc7e50a5090894be8aa5830b58
Atul Kumar <tiwariatul1016@gmail.com>
2026-07-19 14:52:45 +0530

    file repo add

---
+++ Backend/src/repositories/fileRepo.js
@@ -0,0 +1,51 @@
+import { prisma } from "../config/db.js";
+
+
+// Create file metadata in database
+export const create = async (data) => {
+
+    return await prisma.file.create({
+        data
+    });
+
+};
+
+
+// Find file by storage name
+export const findByStoName = async (stoName) => {
+
+    return await prisma.file.findUnique({
+        where: {
+            stoName
+        }
+    });
+
+};
+
+
+// Find all files inside a folder
+export const findAllByFolderId = async (folderId) => {
+    return await prisma.file.findMany({
+        where: {
+            folderId: Number(folderId)
+        },
+        orderBy: {
+            createdAt: "desc"
+        }
+    });
+};
+
+
+// for example :-
+
+// data: {
+//      orgName: "Resume.pdf",
+//      stoName: "ajsh7281.pdf",
+//      mimeType: "application/pdf",
+//      size: 25000,
+//      folderId: 5,
+//      uid: 7
+// }
+//
+// -> Prisma ye query database me chala dega:
+// INSERT INTO File ....
