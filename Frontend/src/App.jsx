10e74f59df1491425f7a9981afae1c9f26fda8dc
ashu670 <abhaylal122@gmail.com>
2026-08-21 22:34:32 +0530

    app.jsx fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/navbar";
-import Signup from "./pages/Signup";
+import Signup from "./pages/signup";
 import Login from "./pages/Login";
 import FolderView from "./pages/Dashboard";
 import AuthSuccess from "./pages/AuthSuccess";
